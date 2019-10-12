59444e8ecb74504ec12a7a6b238172778e570364
wduckitt <william.duckitt@gmail.com>
2019-10-12 12:43:27 +0200

    changed folder structure of styleguide

--- styleguide.config.js
+++ styleguide.config.js
@@ -6,15 +6,13 @@ module.exports = {
       base: 'Roboto'
     }
   },
-  moduleAliases: {
-      'rsg-example': path.resolve(__dirname, 'src')
-    },
+
   styleguideComponents: {
-    Wrapper: path.join(__dirname, './src/automation-studio/src/styleguide/Wrapper'),
-    //LogoRenderer: path.join(__dirname, './src/automation-studio/src/styleguide/components/Logo'),
-//    StyleGuide: path.join(__dirname, './src/automation-studio/src/styleguide/components/StyleGuide'),
-	//	StyleGuideRenderer: path.join(__dirname, './src/automation-studio/src/styleguide/components/StyleGuideRenderer'),
-	//	SectionsRenderer: path.join(__dirname, './src/automation-studio/src/styleguide/components/SectionsRenderer'),
+    Wrapper: path.join(__dirname, './src/React-Automation-Studio/styleguide/Wrapper'),
+    //LogoRenderer: path.join(__dirname, './src/React-Automation-Studio/styleguide/components/Logo'),
+//    StyleGuide: path.join(__dirname, './src/React-Automation-Studio/styleguide/components/StyleGuide'),
+	//	StyleGuideRenderer: path.join(__dirname, './src/React-Automation-Studio/styleguide/components/StyleGuideRenderer'),
+	//	SectionsRenderer: path.join(__dirname, './src/React-Automation-Studio/styleguide/components/SectionsRenderer'),
 
 
 
@@ -22,40 +20,40 @@ module.exports = {
   sections: [
    {
       name: 'Introduction',
-      content: 'src/automation-studio/src/docs/introduction.md'
+      content: 'src/React-Automation-Studio/docs/introduction.md'
    },
 
     {
        name: 'Documentation',
-       content: 'src/automation-studio/src/docs/documentation.md'
+       content: 'src/React-Automation-Studio/docs/documentation.md'
     },
     {
       name: 'Installation Guide',
-      content: 'src/automation-studio/src/docs/installationGuide.md',
+      content: 'src/React-Automation-Studio/docs/installationGuide.md',
       sections: [
         {
           name: 'Installation',
-          content: 'src/automation-studio/src/docs/installation/installation.md',
+          content: 'src/React-Automation-Studio/docs/installation/installation.md',
         },
         {
           name: 'Launching the Docker compose files',
-          content: 'src/automation-studio/src/docs/installation/launching.md',
+          content: 'src/React-Automation-Studio/docs/installation/launching.md',
         },
         {
           name: 'Configuring Enviroment Variables',
-          content: 'src/automation-studio/src/docs/installation/userloginOverview.md',
+          content: 'src/React-Automation-Studio/docs/installation/userloginOverview.md',
           sections: [
             {
               name: ' Enabling login and authentication',
-              content: 'src/automation-studio/src/docs/installation/userlogin.md',
+              content: 'src/React-Automation-Studio/docs/installation/userlogin.md',
             },
             {
               name: ' Enabling user access rights',
-              content: 'src/automation-studio/src/docs/installation/userAccessRights.md',
+              content: 'src/React-Automation-Studio/docs/installation/userAccessRights.md',
             },
             {
               name: ' Enabling HTTPS',
-              content: 'src/automation-studio/src/docs/installation/https.md',
+              content: 'src/React-Automation-Studio/docs/installation/https.md',
             },
           ]
         },
@@ -65,23 +63,23 @@ module.exports = {
       },
   {
     name: 'Style Guide',
-    content: 'src/automation-studio/src/docs/styleguide.md',
+    content: 'src/React-Automation-Studio/docs/styleguide.md',
     sections: [
       {
         name: 'How It Works',
-        content: 'src/automation-studio/src/docs/layout/howItWorks.md',
+        content: 'src/React-Automation-Studio/docs/layout/howItWorks.md',
       },
       {
         name: 'Sample Layouts',
-        content: 'src/automation-studio/src/docs/layout/sampleLayouts.md',
-        components: 'src/automation-studio/src/docs/layout/layoutExamples/*.js',
+        content: 'src/React-Automation-Studio/docs/layout/sampleLayouts.md',
+        components: 'src/React-Automation-Studio/docs/layout/layoutExamples/*.js',
         exampleMode: 'hide', // 'hide' | 'collapse' | 'expand'
         usageMode: 'hide' // 'hide' | 'collapse' | 'expand'
       },
       {
         name: 'Base Components',
        // content: 'docs/ui.md',
-        components: 'src/automation-studio/src/components/BaseComponents/*.js',
+        components: 'src/React-Automation-Studio/components/BaseComponents/*.js',
         exampleMode: 'collapse', // 'hide' | 'collapse' | 'expand'
         usageMode: 'expand' // 'hide' | 'collapse' | 'expand'
       },
