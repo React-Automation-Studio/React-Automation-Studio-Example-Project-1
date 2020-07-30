eca2659a733f2aeeaf280c80f99f82db3f8769a9
William Duckitt <william.duckitt@gmail.com>
2020-07-30 09:15:04 +0200

    organised imports,routes and added comments

--- ReactApp/src/Routes.js
+++ ReactApp/src/Routes.js
@@ -1,98 +1,107 @@
-import React,{useContext} from 'react';
+import React, { useContext } from 'react';
 
 import { Route, BrowserRouter, Switch } from 'react-router-dom'
 
-import Main from './Main';
-import MainPublic from './MainPublic';
-import Demos from './components/demos/Demos';
 
-import Help from './React-Automation-Studio/components/docs/Help';
+// demos
+import MainDashboard from './React-Automation-Studio/components/UI/MainDashboard';
+import Demos from './components/demos/Demos';
+import EpicsDemos from './React-Automation-Studio/components/Examples/EpicsDemos';
+import MobileDemo1 from './React-Automation-Studio/components/Examples/Mobile/MobileDemo1';
+import MobileDemo2 from './React-Automation-Studio/components/Examples/Mobile/MobileDemo2';
+import BeamlineControlSystem from './React-Automation-Studio/components/ControlScreens/BeamlineControlSystem';
+import TableControlSystem from './React-Automation-Studio/components/ControlScreens/TableControlSystem';
+import Test3D from './React-Automation-Studio/components/Experimental/Test3D';
+import AlarmHandlerDemo from './React-Automation-Studio/components/Examples/AlarmHandlerDemo';
+import LoadSaveExample from './React-Automation-Studio/components/ExperimentalExamples/LoadSaveExample';
+import SettingsSteererXY from './React-Automation-Studio/components/SettingsPages/SettingsSteererXY';
+import SettingsSinglePS from './React-Automation-Studio/components/SettingsPages/SettingsSinglePS';
+import AdvancedSettingsSinglePS from './React-Automation-Studio/components/ExperimentalControlScreens/SettingsPages/AdvancedSettingsSinglePS';
+import Vault from './React-Automation-Studio/components/AlarmHandler/Vault';
+//staging
 import Staging from './components/staging/Staging';
 import Example from './components/staging/Example/Example';
 import Example1 from './components/staging/Example/Example1';
 import Example2 from './components/staging/Example/Example2';
 import Example3 from './components/staging/Example/Example3';
-
-import Probe from './React-Automation-Studio/components/SettingsPages/Probe';
-
+//system
 import LogIn from './React-Automation-Studio/LogIn';
-
-
-import EpicsDemos from './React-Automation-Studio/components/Examples/EpicsDemos';
-
-import MobileDemo2 from './React-Automation-Studio/components/Examples/Mobile/MobileDemo2';
-import MobileDemo1 from './React-Automation-Studio/components/Examples/Mobile/MobileDemo1';
-
-import ControlTestHarp1 from './React-Automation-Studio/components/ControlScreens/ControlTestHarp1';
-
-import ControlTableExample from './React-Automation-Studio/components/ControlScreens/ControlTableExample';
-import ComponentsWithMultiplePVs from './React-Automation-Studio/components/Examples/ComponentsWithMultiplePVs';
-import Test3D from './React-Automation-Studio/components/Experimental/Test3D';
-
-import SettingsSteererXY from './React-Automation-Studio/components/SettingsPages/SettingsSteererXY';
-import SettingsSinglePS from './React-Automation-Studio/components/SettingsPages/SettingsSinglePS';
+import Probe from './React-Automation-Studio/components/SettingsPages/Probe';
+import Help from './React-Automation-Studio/components/docs/Help';
+import Main from './Main';
+import MainPublic from './MainPublic';
 import AutomationStudioContext from 'React-Automation-Studio/components/SystemComponents/AutomationStudioContext';
-import MainDashboard from './React-Automation-Studio/components/UI/MainDashboard';
-import BeamlineControlSystem from './React-Automation-Studio/components/ExperimentalControlScreens/BeamlineControlSystem';
+
+
 const Routes = (props) => {
   const context = useContext(AutomationStudioContext);
   /* eslint-disable-next-line no-unused-vars */
-  const userData=context.userData;
+  const userData = context.userData;
   /* eslint-disable-next-line no-unused-vars */
   const roles = context.userData.roles;
   const username = context.userData.username;
   let loggedIn = username !== "" || process.env.REACT_APP_EnableLogin !== 'true';
   return (
-  <BrowserRouter >
+    <BrowserRouter >
 
-    <Switch>
+      <Switch>
 
-    <Route exact path="/" >
+        {/*system start*/}
+        <Route exact path="/" >
           {loggedIn === false
             ? <MainPublic />
-            :<Main/>
+            : <Main />
           }
-    </Route>
+        </Route>
 
-      {process.env.REACT_APP_EnableLogin==='true'&&
-        <Route exact path="/LogIn" component={ LogIn } />
-      }
+        {process.env.REACT_APP_EnableLogin === 'true' &&
+          <Route exact path="/LogIn" component={LogIn} />
+        }
 
-      <Route path="/Probe" component={Probe} />
-      <Route path="/DemosDashboard" component={MainDashboard} />
-      <Route path="/Demos" component={Demos} />
-      <Route path="/Help" component={Help} />
-      <Route path="/Staging" component={Staging} />
+        <Route path="/Probe" component={Probe} />
+        <Route path="/Help" component={Help} />
+        {/*system end*/}
 
-      <Route path="/Example" component={Example} />
-      <Route path="/Example1" component={Example1} />
-      <Route path="/Example2" component={Example2} />
-      <Route path="/Example3" component={Example3} />
+        {/*demos start*/}
+        <Route path="/DemosDashboard" component={MainDashboard} />
+        <Route path="/Demos" component={Demos} />
+        <Route path="/MobileDemo1" component={MobileDemo1} />
+        <Route path="/MobileDemo2" component={MobileDemo2} />
+        <Route path="/EpicsDemos" component={EpicsDemos} />
+        <Route path="/Test3D" component={Test3D} />
+        <Route path="/AlarmHandlerDemo" component={AlarmHandlerDemo} />
+        <Route path="/VaultDemo" component={Vault} />
+        <Route path="/LoadSaveExample" component={LoadSaveExample} />
+        {/* new Beamline and table control System routes start*/}
+        <Route path="/BeamlineControlSystem" component={BeamlineControlSystem} />
+        <Route path="/AdvancedSettingsSinglePS" component={AdvancedSettingsSinglePS} />
+        <Route path="/TableControlSystem" component={TableControlSystem} />
+        <Route path="/SettingsSinglePS" component={SettingsSinglePS} />
+        <Route path="/SettingsSteererXY" component={SettingsSteererXY} />
+        {/* new Beamline and table control System routes end*/}
 
-      <Route path="/SettingsSinglePS" component={SettingsSinglePS} />
-      <Route path="/SettingsSteererXY" component={SettingsSteererXY} />
-      <Route path="/Probe" component={Probe} />
+         {/*demos end*/}
 
-      <Route path="/MobileDemo2" component={MobileDemo2} />
-      <Route path="/MobileDemo1" component={MobileDemo1} />
+         {/*staging start*/}
+         <Route path="/Staging" component={Staging} />
+        <Route path="/Example" component={Example} />
+        <Route path="/Example1" component={Example1} />
+        <Route path="/Example2" component={Example2} />
+        <Route path="/Example3" component={Example3} />
+        {/*staging end*/}
 
-      <Route path="/ControlTestHarp1" component={ControlTestHarp1} />
-
-      <Route path="/ControlTableExample" component={ControlTableExample} />
-      <Route path="/BeamlineControlSystem" component={BeamlineControlSystem} />
-      <Route path="/EpicsDemos" component={EpicsDemos} />
+       
 
 
-      <Route path="/Test3D" component={Test3D} />
-      <Route path="/ComponentsWithMultiplePVs" component={ComponentsWithMultiplePVs} />
+     
 
 
 
 
 
-    </Switch>
+      </Switch>
 
-  </BrowserRouter>
+    </BrowserRouter>
   )
 }
 export default Routes;
