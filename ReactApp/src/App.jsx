7d52c15a96f619458d602878dea2f8b5ec8f193d
William Duckitt <wdduckitt@sun.ac.za>
2024-01-24 17:24:13 +0200

    rename

--- ReactApp/src/App.jsx
+++ ReactApp/src/App.jsx
@@ -2,13 +2,15 @@
 import React from 'react';
 import RasAppCore from 'React-Automation-Studio/components/SystemComponents/RasAppCore';
 import themes  from 'React-Automation-Studio/components/UI/Themes/themes';
-import Routes from './Routes';
+import AppRoutes from "./AppRoutes";
 
 const App = (props) => {
   return (
-    <RasAppCore  themes={themes} defaultTheme={'Ocean'}   >
-      <Routes />
-    </RasAppCore>
+     <RasAppCore  themes={themes} defaultTheme={'Ocean'}   >
+       <AppRoutes /> 
+     
+     </RasAppCore>
+    // <div>hello</div>
   );
 }
 
