cd58cdd10dcb166fdc61901cf5a1b7c6c1299b70
William Duckitt <wdduckitt@sun.ac.za>
2024-01-26 09:34:32 +0200

    Update React-Automation-Studio alias path

--- ReactApp/vite.config.js
+++ ReactApp/vite.config.js
@@ -5,7 +5,7 @@ import { resolve, dirname } from "node:path";
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    alias: [{ find: "React-Automation-Studio", replacement: resolve(__dirname, "./src/React-Automation-Studio") }]
+    alias: [{ find: "React-Automation-Studio", replacement: resolve(__dirname, "./React-Automation-Studio") }]
   },
   server: {
     port: 3000,
