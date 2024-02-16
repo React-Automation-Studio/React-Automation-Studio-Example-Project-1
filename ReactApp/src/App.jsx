cd9566d51a589222e7d7c7db551ff26a45ae23b6
William Duckitt <wdduckitt@sun.ac.za>
2024-02-16 09:25:59 +0200

    Refactor App component

--- ReactApp/src/App.jsx
+++ ReactApp/src/App.jsx
@@ -1,19 +1,14 @@
-
-import React from 'react';
-import RasAppCore from 'React-Automation-Studio/components/SystemComponents/RasAppCore';
-import themes  from 'React-Automation-Studio/components/UI/Themes/themes';
+import React from "react";
+import RasAppCore from "React-Automation-Studio/components/SystemComponents/RasAppCore";
+import themes from "React-Automation-Studio/components/UI/Themes/themes";
 import AppRoutes from "./AppRoutes";
 
 const App = (props) => {
   return (
-     <RasAppCore  themes={themes} defaultTheme={'Navy'}   >
-       <AppRoutes /> 
-     
-     </RasAppCore>
-    // <div>hello</div>
+    <RasAppCore themes={themes} defaultTheme={"Navy"}>
+      <AppRoutes />
+    </RasAppCore>
   );
-}
-
-
+};
 
 export default App;
