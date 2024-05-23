b07d0fb29e1a0a2f43d83ba0035b839137d74ab6
William Duckitt <wdduckitt@sun.ac.za>
2024-05-23 14:35:58 +0200

    chore: Remove unused PropTypes in Component1.jsx

--- ReactApp/src/components/customComponents/Component1.jsx
+++ ReactApp/src/components/customComponents/Component1.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import PropTypes from "prop-types";
 /**
  * Example component
  */
@@ -7,12 +6,6 @@ const Component1 = (props) => {
   return <div>My Example</div>;
 };
 
-Component1.propTypes = {
-  /** Define your custom Props*/
-  customProp: PropTypes.string,
-};
-Component1.defaultProps = {
-  customProp: "Hello world",
-};
+
 
 export default Component1;
